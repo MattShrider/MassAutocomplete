f441e24056fa2fe332f11e02064b9ee4052d42d5
Matthew Shrider <mattshrider@gmail.com>
2014-12-12 10:09:06 -0500

    Fixes #13 - Use position relative to parent instead of document

--- massautocomplete.js
+++ massautocomplete.js
@@ -18,6 +18,7 @@ angular.module('MassAutoComplete', [])
       '</div>',
     link: function (scope, element) {
       scope.container = angular.element(element[0].getElementsByClassName('ac-container')[0]);
+      scope.element = element;
     },
     controller: ["$scope", function ($scope) {
       var that = this;
@@ -71,12 +72,11 @@ angular.module('MassAutoComplete', [])
 
       function _position_autocomplete() {
         var rect = current_element[0].getBoundingClientRect(),
-            scrollTop = $document[0].body.scrollTop || $document[0].documentElement.scrollTop || $window.pageYOffset,
-            scrollLeft = $document[0].body.scrollLeft || $document[0].documentElement.scrollLeft || $window.pageXOffset,
+            parentOffset = angular.element($scope.element[0].offsetParent)[0].getBoundingClientRect(),
             container = $scope.container[0];
 
-        container.style.top = rect.top + rect.height + scrollTop + 'px';
-        container.style.left = rect.left + scrollLeft + 'px';
+        container.style.top = rect.top + rect.height - parentOffset.top + 'px';
+        container.style.left = rect.left - parentOffset.left + 'px';
         container.style.width = rect.width + 'px';
       }
       var position_autocomplete = debounce(_position_autocomplete, user_options.debounce_position);
